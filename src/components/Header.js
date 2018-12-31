07e11d1ebde67476bb6e35b9116ef41a87dcafa4
eastflag <eastflag@gmail.com>
2018-12-31 14:42:30 +0900

    Stopwatch view

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Stats from "./Stats";
+import Stopwatch from "./Stopwatch";
 
 const Header = (props) => {
   // console.log(props);
@@ -7,6 +8,7 @@ const Header = (props) => {
     <header>
       <Stats players={props.players} />
       <h1>{ props.title }</h1>
+      <Stopwatch />
     </header>
   )
 }
