1a487d99047137f2f61936079a7e7e0e3b296afb
eastflag <eastflag@gmail.com>
2018-12-31 15:23:19 +0900

    Stopwatch - start stop 구현

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
 
 class Stopwatch extends Component {
+  state = {
+    isRunning: false
+  }
+  
+  handleStopwatch = () => {
+    this.setState(prevState => {
+      return {isRunning: !prevState.isRunning}
+    });
+  }
+  
   render() {
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
         <span className="stopwatch-time">0</span>
-        <button>Start</button>
+        <button onClick={this.handleStopwatch}>{this.state.isRunning ? 'Start' : 'Stop'}</button>
         <button>Reset</button>
       </div>
     )
