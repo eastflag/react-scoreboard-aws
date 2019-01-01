45208b98182fc9654302ca0abb9db84f0967522a
eastflag <eastflag@gmail.com>
2019-01-02 01:54:50 +0900

    Stopwatch - 로직 구현

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,23 +1,42 @@
 import React, {Component} from 'react';
 
 class Stopwatch extends Component {
+  tickRef;
+  
   state = {
-    isRunning: false
+    isRunning: false,
+    timer: 0
+  }
+  
+  componentDidMount() {
+    this.tickRef = setInterval(() => this.tick(), 1000);
+  }
+  
+  componentWillUnmount() {
+    clearInterval(this.tickRef);
+  }
+  
+  tick() {
+    if (this.state.isRunning) {
+      this.setState(prevState => prevState.timer += 1);
+    }
   }
   
   handleStopwatch = () => {
-    this.setState(prevState => {
-      return {isRunning: !prevState.isRunning}
-    });
+    this.setState(prevState => ({isRunning: !prevState.isRunning}));
+  }
+  
+  handleReset = () => {
+    this.setState({timer: 0});
   }
   
   render() {
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
-        <span className="stopwatch-time">0</span>
-        <button onClick={this.handleStopwatch}>{this.state.isRunning ? 'Start' : 'Stop'}</button>
-        <button>Reset</button>
+        <span className="stopwatch-time">{this.state.timer}</span>
+        <button onClick={this.handleStopwatch}>{this.state.isRunning ? 'Stop' : 'Start'}</button>
+        <button onClick={this.handleReset}>Reset</button>
       </div>
     )
   }
