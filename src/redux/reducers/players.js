a61fefbce44604375a0809480dabdb8ae927ccbb
eastflag <eastflag@gmail.com>
2019-01-14 15:14:25 +0900

    refactoring

--- src/redux/reducers/players.js
+++ src/redux/reducers/players.js
@@ -1,4 +1,4 @@
-import {ADD_PLAYER, CHANGE_SCORE, REMOVE_PLAYER, UPDATE_TITLE} from "./actionTypes";
+import {ADD_PLAYER, CHANGE_SCORE, REMOVE_PLAYER, UPDATE_TITLE} from "../actionTypes";
 
 let playerId = 4;
 
