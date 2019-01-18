d28d38b6eb8929ff79479d149af391692131eba0
eastflag <eastflag@gmail.com>
2019-01-18 11:17:21 +0900

    navbar - reactstrap 모듈 추가, navbar 컴포넌트처리

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,29 +1,68 @@
 import React, {Component} from 'react';
+import {
+  Collapse, DropdownItem, DropdownMenu,
+  DropdownToggle,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  UncontrolledDropdown
+} from "reactstrap";
 import {NavLink} from "react-router-dom";
 
 class Menu extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <NavLink className="navbar-brand order-1" to="/">React</NavLink>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/heroes">Heroes</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/scoreboard">Scoreboard</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/todo">Todo</NavLink>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <Navbar color="dark" dark expand="md">
+        <NavLink to="/" className="navbar-brand">React</NavLink>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink to="/heroes" className="nav-link">Heroes</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/scoreboard" className="nav-link">Scoreboard</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/todo" className="nav-link">Todo</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </Navbar>
     )
   }
 }
