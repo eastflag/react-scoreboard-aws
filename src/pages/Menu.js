2fb762ba9e6441dce386bff2bb416ada3a31ada2
eastflag <eastflag@gmail.com>
2019-01-18 09:57:40 +0900

    navbar - bootstrap 버전 변경

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -4,17 +4,25 @@ import {NavLink} from "react-router-dom";
 class Menu extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-between">
-        <NavLink className="navbar-brand" to="/">React</NavLink>
-
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/scoreboard">Scoreboard</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/heroes">heroes</NavLink>
-          </li>
-        </ul>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <NavLink className="navbar-brand order-1" to="/">React</NavLink>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/heroes">Heroes</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/scoreboard">Scoreboard</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/todo">Todo</NavLink>
+            </li>
+          </ul>
+        </div>
       </nav>
     )
   }
