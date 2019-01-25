a06be51ae5a489506db3a11ccc72fb418b6d0c3d
eastflag <eastflag@gmail.com>
2019-01-25 15:37:53 +0900

    반응형 - card에 masonry 알고리즘 적용

--- src/pages/heroes/Heroes.js
+++ src/pages/heroes/Heroes.js
@@ -21,17 +21,15 @@ class Heroes extends Component {
   
   render() {
     return (
-      <div className="row">
+      <div className="card-columns">
         {this.state.heroes.map(hero => (
-          <div className="col-6 col-sm-4 col-md-3 p-1 p-sm-2 p-md-3" key={hero.hero_id}>
-            <div className="card">
-              <img src={hero.photo ? hero.photo : process.env.PUBLIC_URL + '/images/baseline-face-24px.svg'}
-                   style={{width: '100%'}} alt={hero.name}></img>
-              <div className="card-body">
-                <h5 className="card-title">{hero.name}</h5>
-                <p className="card-text">email: {hero.email}</p>
-                <p className="card-text">sex: {hero.sex}</p>
-              </div>
+          <div className="card" key={hero.hero_id}>
+            <img src={hero.photo ? hero.photo : process.env.PUBLIC_URL + '/images/baseline-face-24px.svg'}
+                 style={{width: '100%'}} alt={hero.name}></img>
+            <div className="card-body">
+              <h5 className="card-title">{hero.name}</h5>
+              <p className="card-text">email: {hero.email}</p>
+              <p className="card-text">sex: {hero.sex}</p>
             </div>
           </div>
         ))}
