6228eef1b0f0077ee5b7e9e577fef6f92751b95d
eastflag <eastflag@gmail.com>
2019-01-30 09:40:41 +0900

    Heroes sub menu 구성

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -2,8 +2,8 @@ import React, {Component} from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from "./Home";
 import Scoreboard from "./scoreboard/Scoreboard";
-import Heroes from "./heroes/Heroes";
 import Menu from "./Menu";
+import {Index} from "./heroes/Index";
 
 export class Root extends Component {
   render() {
@@ -14,7 +14,7 @@ export class Root extends Component {
           <div className="container py-3" style={{backgroundColor: '#ffffff'}}>
             <Switch>
               <Route exact path="/" component={Home}></Route>
-              <Route path="/heroes" component={Heroes}></Route>
+              <Route path="/heroes" component={Index}></Route>
               <Route path="/scoreboard" component={Scoreboard}></Route>
             </Switch>
           </div>
