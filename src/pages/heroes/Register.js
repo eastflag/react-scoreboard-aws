bfc06571c7a9c87b37cd10c8f0e8e16bc8d40627
eastflag <eastflag@gmail.com>
2019-02-01 20:53:52 +0900

    hero 등록 - 바인딩하기

--- src/pages/heroes/Register.js
+++ src/pages/heroes/Register.js
@@ -1,6 +1,42 @@
 import React, {Component} from 'react';
 
 export class Register extends Component {
+  state = {
+    name: '',
+    email: '',
+    sex: {
+      male: false,
+      female: false
+    },
+    country: '',
+    address: '',
+    power: {
+      flying: false,
+      penetration: false,
+      hacking: false,
+      strength: false
+    }
+  }
+  
+  handleText = (e, key) => {
+    this.setState({[key]: e.target.value});
+  }
+  
+  handleSex = (e) => {
+    const sex = {
+      male: false,
+      female: false
+    }
+    sex[e.target.value] = e.target.checked;
+    this.setState({sex});
+  }
+  
+  handlePower = (e) => {
+    const power = {...this.state.power};
+    power[e.target.value] = e.target.checked;
+    this.setState({power});
+  }
+  
   render() {
     return (
       <>
@@ -9,23 +45,27 @@ export class Register extends Component {
         <form>
           <div className="form-group mt-1">
             <label htmlFor="name">Name</label>
-            <input type="text" className="form-control" placeholder="Enter Name" id="name" />
+            <input type="text" className="form-control" placeholder="Enter Name" id="name"
+                   value={this.state.name} onChange={(e) => this.handleText(e, 'name')} />
           </div>
           
           <div className="form-group mt-1">
             <label htmlFor="email">Email Address</label>
-            <input type="email" className="form-control" placeholder="Enter Email" id="email" />
+            <input type="email" className="form-control" placeholder="Enter Email" id="email"
+                   value={this.state.email} onChange={(e) => this.handleText(e, 'email')} />
           </div>
           
           <div className="d-flex flex-column mt-1">
             <div>성별</div>
             <div>
               <div className="form-check form-check-inline">
-                <input className="form-check-input" type="radio" name="sex" value="male" id="male" />
+                <input className="form-check-input" type="radio" name="sex" value="male" id="male"
+                       checked={this.state.sex.male} onChange={this.handleSex}/>
                 <label className="form-check-label" htmlFor="male">남자</label>
               </div>
               <div className="form-check form-check-inline">
-                <input className="form-check-input" type="radio" name="sex" value="female" id="female" />
+                <input className="form-check-input" type="radio" name="sex" value="female" id="female"
+                       checked={this.state.sex.female} onChange={this.handleSex} />
                 <label className="form-check-label" htmlFor="female">여자</label>
               </div>
             </div>
@@ -33,7 +73,8 @@ export class Register extends Component {
   
           <div className="form-group mt-1">
             <label htmlFor="country">country</label>
-            <select className="form-control" id="country">
+            <select className="form-control" id="country" value={this.state.country} onChange={(e)=>this.handleText(e, 'country')}>
+              <option value=""></option>
               <option value="Japan">Japan</option>
               <option value="American">American</option>
               <option value="Korean">Korean</option>
@@ -42,26 +83,31 @@ export class Register extends Component {
           
           <div className="form-group mt-1">
             <label htmlFor="address">Address</label>
-            <textarea className="form-control" placeholder="Enter address" id="address" rows="3"></textarea>
+            <textarea className="form-control" placeholder="Enter address" id="address" rows="3"
+                      value={this.state.address} onChange={(e)=>this.handleText(e, 'address')}></textarea>
           </div>
           
           <div className="d-flex flex-column mt-1">
             <div>power</div>
             <div>
               <div className="form-check form-check-inline">
-                <input type="checkbox" className="form-check-input" id="flying" />
+                <input type="checkbox" value="flying" className="form-check-input" id="flying"
+                       checked={this.state.power.flying} onChange={this.handlePower}/>
                 <label className="form-check-label" htmlFor="flying">flying</label>
               </div>
               <div className="form-check form-check-inline">
-                <input type="checkbox" className="form-check-input" id="penetration" />
+                <input type="checkbox" value="penetration" className="form-check-input" id="penetration"
+                       checked={this.state.power.penetration} onChange={this.handlePower} />
                 <label className="form-check-label" htmlFor="penetration">penetration</label>
               </div>
               <div className="form-check form-check-inline">
-                <input type="checkbox" className="form-check-input" id="hacking" />
+                <input type="checkbox" value="hacking" className="form-check-input" id="hacking"
+                       checked={this.state.power.hacking} onChange={this.handlePower}/>
                 <label className="form-check-label" htmlFor="hacking">hacking</label>
               </div>
               <div className="form-check form-check-inline">
-                <input type="checkbox" className="form-check-input" id="strength" />
+                <input type="checkbox" value="strength" className="form-check-input" id="strength"
+                       checked={this.state.power.strength} onChange={this.handlePower} />
                 <label className="form-check-label" htmlFor="strength">strength</label>
               </div>
             </div>
@@ -72,6 +118,10 @@ export class Register extends Component {
           </div>
         
         </form>
+        
+        <p>
+          {JSON.stringify(this.state)}
+        </p>
       </>
     )
   }
