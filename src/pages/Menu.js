b717ba84d42414762021cf651346c63cd3ac4e14
eastflag <eastflag@gmail.com>
2019-12-03 21:29:51 +0900

    Menu 토글 부분 useState로 변경

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,4 +1,5 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
+
 import {
   Collapse, DropdownItem, DropdownMenu,
   DropdownToggle,
@@ -10,62 +11,49 @@ import {
 } from "reactstrap";
 import {NavLink} from "react-router-dom";
 
-class Menu extends Component {
-  constructor(props) {
-    super(props);
-    
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-  
-  render() {
-    return (
-      <Navbar color="dark" dark expand="md">
-        <div className="container">
-          <NavLink to="/" className="navbar-brand">React</NavLink>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink to="/heroes" className="nav-link">Heroes</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink to="/scoreboard" className="nav-link">Scoreboard</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink to="/product" className="nav-link">Product</NavLink>
-              </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-          </Collapse>
-        </div>
-      </Navbar>
-    )
-  }
+const Menu = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <Navbar color="dark" dark expand="md">
+      <div className="container">
+        <NavLink to="/" className="navbar-brand">React</NavLink>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink to="/heroes" className="nav-link">Heroes</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/scoreboard" className="nav-link">Scoreboard</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink to="/product" className="nav-link">Product</NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  Option 1
+                </DropdownItem>
+                <DropdownItem>
+                  Option 2
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  Reset
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Collapse>
+      </div>
+    </Navbar>
+  )
 }
 
 export default Menu;
