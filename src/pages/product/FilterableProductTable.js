2a5ebdd84234f40010bc14768a53d86ccba74921
eastflag <eastflag@gmail.com>
2019-12-03 15:30:34 +0900

    .

--- src/pages/product/FilterableProductTable.js
+++ src/pages/product/FilterableProductTable.js
@@ -2,26 +2,22 @@ import React, {Component} from 'react';
 import {SearchBar} from "./SearchBar";
 import {ProductTable} from "./ProductTable";
 
-class FilterableProductTable extends Component {
-	state = {
-		products: [
-			{category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
-			{category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
-			{category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
-			{category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
-			{category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
-			{category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
-		]
-	}
-	
-	render() {
-		return (
-			<div>
-				<SearchBar/>
-				<ProductTable products={this.state.products}></ProductTable>
-			</div>
-		)
-	}
+const products = [
+	{category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+	{category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+	{category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+	{category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+	{category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+	{category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+]
+
+const FilterableProductTable = () => {
+	return (
+		<div>
+			<SearchBar/>
+			<ProductTable products={products}></ProductTable>
+		</div>
+	)
 }
 
 export default FilterableProductTable;
